bfdf2cdba32eaae8a7f2875d8772a2c6101764e4
Ted Staros <tstaros23@gmail.com>
2023-12-26 21:33:55 -0600

    first commit

---
+++ remove_duplicates.rb
@@ -0,0 +1,38 @@
+def remove_duplicates(nums)
+    i = 0
+    var = 0
+    k = 0
+    while i < nums.length do
+      if i != 0 && nums[i] == var
+        nums[i] = nil
+        i += 1
+      else
+        var = nums[i]
+        k += 1
+        i += 1
+      end
+    end
+
+    current_index = 0
+    counter = 0
+    j = 0
+    while j < nums.length do
+      if nums[j] != nil && counter != j
+        nums[counter] = nums[j]
+
+        nums[j] = nil
+        counter += 1
+      else if nums[j] != nil
+        counter += 1
+        j += 1
+      else
+        j += 1
+      end
+    end
+  end
+  return k
+end
+nums = [0,0,1,1,1,2,2,3,3,4]
+nums2 = [1,1,2]
+nums3 = [1,2]
+remove_duplicates(nums2)
